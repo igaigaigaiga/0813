bb645a434de9dea4cf558a5f21b69e65e7df76fc
igaigaigaiga <staff-tokyoonline@iga-group.com>
2025-08-13 16:30:21 +0900

    Add files via upload

---
+++ js/base_chino.js
@@ -0,0 +1,170 @@
+
+
+jQuery(function($){
+
+	$('.slider').slick({
+	fade:true,//切り替えをフェードで行う。初期値はfalse。
+		autoplay: true,//自動的に動き出すか。初期値はfalse。
+		autoplaySpeed: 2000,//次のスライドに切り替わる待ち時間
+		speed:2000,//スライドの動きのスピード。初期値は300。
+		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+		slidesToShow: 1,//スライドを画面に3枚見せる
+		slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+		arrows: false,//左右の矢印あり
+		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+		dots: false,//下部ドットナビゲーションの表示
+        pauseOnFocus: false,//フォーカスで一時停止を無効
+        pauseOnHover: false,//マウスホバーで一時停止を無効
+        pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+    });
+
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+    $('.slider').slick('slickPlay');
+});
+	
+$(function(){
+  $(".md-btn").each(function(){
+    $(this).on('click',function(e){
+      e.preventDefault();
+      var target = $(this).data('target');
+      var modal = document.getElementById(target);
+      $(modal).find('.md-overlay,.md-contents').fadeIn();
+    });
+  });
+  $('.md-close').on('click',function(){
+    $('.md-overlay,.md-contents').fadeOut();
+  });
+});
+
+$(function(){
+  $(window).scroll(function (){
+    $(".wataame").each(function(){
+      var imgPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + windowHeight/4){
+        $(this).addClass("fade_on");
+      } else {
+        $(this).removeClass("fade_on");
+      }
+    });
+  });
+});
+
+	
+	// ふわっ
+
+	$(function fadeAnime(){
+    $(window).scroll(function (){
+         $('.fadeInTrigger').each(function(){ //fadeInTriggerというクラス名が
+    var elemPos = $(this).offset().top-50;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeIn');// 画面内に入ったらfadeInというクラス名を追記
+    }else{
+    $(this).removeClass('fadeIn');// 画面外に出たらfadeInというクラス名を外す
+    }
+    });
+	
+    });
+});
+	
+
+	});
+
+
+	$('#page-link a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+	var pos = $(elmHash).offset().top;	//idの上部の距離を取得
+	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+	return false;
+});
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
+});
+
+//上部画像の設定
+$('.gallery').slick({
+	infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+	fade: true, //フェードの有効化
+	arrows: true,//左右の矢印あり
+	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+	asNavFor: '.choice-btn', //連動させるサムネイルのクラス名
+});
+
+//選択画像の設定
+$('.choice-btn').slick({
+	infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+	slidesToShow: 3, //表示させるスライドの数
+	focusOnSelect: true, //フォーカスの有効化
+	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+	asNavFor: '.gallery', //連動させるスライドショーのクラス名
+});
+
+
+
+
+//スクロールをするたびにアニメーションを行う設定  
+$('.fadeInUpTrigger').on('inview', function(event, isInView) {
+    if (isInView) {//表示領域に入った時
+      $(this).addClass('animate__animated animate__fadeInUp');//クラス名が付与
+    } else {//表示領域から出た時
+      $(this).removeClass('animate__animated animate__fadeInUp');//クラス名が除去
+    }
+  });
+
+//スクロールをしたら1度だけアニメーションをする設定
+  $('.fadeInUpTriggerOnce').on('inview', function(event, isInView) {
+    if (isInView) {//表示領域に入った時
+      $(this).addClass('animate__animated animate__fadeInUp');//クラス名が付与
+    }
+  });
+
+//違う動きを追加設定  
+$('.fadeInDownTrigger').on('inview', function(event, isInView) {
+    if (isInView) {//表示領域に入った時
+      $(this).addClass('animate__animated animate__fadeInDown');//クラス名が付与
+    } else {//表示領域から出た時
+      $(this).removeClass('animate__animated animate__fadeInDown');//クラス名が除去
+    }
+  });
+
+
+
+// 動きのきっかけの起点となるアニメーションの名前を定義
+function fadeAnime(){
+
+	// ふわっ
+	$('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+		var elemPos = $(this).offset().top-50;//要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('fadeRight');// 画面内に入ったらfadeUpというクラス名を追記
+		}else{
+		$(this).removeClass('fadeRight');// 画面外に出たらfadeUpというクラス名を外す
+		}
+		});
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+	$(window).scroll(function (){
+		fadeAnime();/* アニメーション用の関数を呼ぶ*/
+	});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+	$(window).on('load', function(){
+		fadeAnime();/* アニメーション用の関数を呼ぶ*/
+	});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
